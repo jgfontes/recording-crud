a7d31162b663c269d5830131924b78649479e0f2
Julio Conta Pessoal <ju.1996@outlook.com>
2023-03-12 16:58:33 -0300

    Initialize Spring Boot Project

---
+++ src/main/java/com/jgfontes/recordingcrud/RecordingCrudApplication.java
@@ -0,0 +1,13 @@
+package com.jgfontes.recordingcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordingCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordingCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jgfontes/recordingcrud/RecordingCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgfontes.recordingcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordingCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
